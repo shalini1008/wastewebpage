9326e3bbdc5911036ce076266405247b12e76313
shalini_10080 <shalini7217625537@gmail.com>
2025-01-08 19:14:04 +0530

    first commit

---
+++ src/Component/Layout.jsx
@@ -0,0 +1,17 @@
+import Navbar from './Header';
+import Footer from './Footer';
+
+// eslint-disable-next-line react/prop-types
+function Layout({ children }) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Layout from "../Component/Layout"
+
+
+function Home() {
+  return (
+    <Layout>
+        <div>
+            Herosection
+        </div>
+    </Layout>
+  )
+}
+
+export default Home
