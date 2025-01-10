e89e80a5e5dbe8cbff9c991c94acb380b4d0e7d9
shalini_10080 <shalini7217625537@gmail.com>
2025-01-10 21:51:58 +0530

    profile and history of user

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import Home from './pages/Home'
 import Product from './pages/Product';
 import SnapNsell from './pages/snapNsell/SnapNsell';
+import UserProfile from './pages/user/UserProfile';
+import History from './pages/user/History';
 // import Login from './pages/Login'
 // import Signup from './pages/Signup'
 
@@ -14,6 +16,8 @@ function App() {
           <Route path="/" element={<Home/>}/>
           <Route path="/products" element={<Product/>}/>
           <Route path="/snapNsell" element={<SnapNsell/>}/>
+          <Route path="/profile" element={<UserProfile/>}/>
+          <Route path="/profileHistory" element={<History/>}/>
         </Routes>
       </Router>
     </>
